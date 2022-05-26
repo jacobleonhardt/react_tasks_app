f6515c3d67993dac8431ff6af5e52fccb8697dd1
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-05-26 13:20:03 -0500

    create Header component and add basic style

---
+++ task-tracker/src/components/header/Header.js
@@ -0,0 +1,12 @@
+import './header.scss'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Task Trackr</h1>
+      <button className="header__btn">Add</button>
+    </header>
+  )
+}
+
+export default Header
