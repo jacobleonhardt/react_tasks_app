9ad259f4139578aa1a29d3836c8794b213b5a6bb
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-05-27 15:15:38 -0500

    basic styling for about page

--- task-tracker/src/components/about/About.js
+++ task-tracker/src/components/about/About.js
@@ -1,9 +1,10 @@
+import './about.scss'
 import { Link } from "react-router-dom"
 
 const About = () => {
   return (
     <section>
-      <h4>About</h4>
+      <h1>About</h1>
       <p>Version 1.0.0</p>
       <Link to='/'>Go Back</Link >
     </section>
